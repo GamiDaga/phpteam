4ef19e382a7bb5f7d1aa65834f4e2ae9fb3b4746
GamiDaga <mati955.daga@gmail.com>
2017-03-28 22:48:30 -0300

    inicio del proyecto y agregado de un navbar en layout

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once('./layout.php');
+ ?>
+<head>
+
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <div class="contenedor">
+        
+    </div>
+
+</body>
+
+<footer>
+
+</footer>
---
+++ layout.php
@@ -0,0 +1,37 @@
+<head>
+    <!--Importacion de Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!---->
+    <link type="text/css" rel="stylesheet" href="./css/materialize.min.css"  media="screen,projection"/>
+    <!-- Importcion de hoja de estilos propia -->
+    <link rel="stylesheet" href="/css/stylesheet.css">
+</head>
+
+<body>
+
+      <nav>
+        <div class="ddd">
+
+        <div class="nav-wrapper ddd">
+          <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="sass.html"><i class="material-icons">search</i></a></li>
+            <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
+            <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
+            <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+          </ul>
+        </div>
+    </div>
+      </nav>
+    <header>
+    </header>
+
+
+
+<script type="text/javascript" src="./js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="./js/materialize.min.js"></script>
+</body>
+
+<footer>
+
+</footer>
