ad213c781c8f6b5100f2fdc00d1323ee0a2cf3f4
GamiDaga <mati955.daga@gmail.com>
2017-03-31 13:41:55 -0300

    commit test

--- index.php
+++ index.php
@@ -10,21 +10,10 @@
         </div>
             <div class="section white">
                 <div class="row container">
-                <h2 class="header">Parallax</h2>
-                <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+                <h2 class="header">asdasdasdasd</h2>
+                <p class="grey-text text-darken-3 lighten-3"></p>
                 </div>
             </div>
-            <div class="parallax-container">
-                <div class="parallax"><img src="images/parallax2.jpg"></div>
-        </div>
+
 
 </body>
-
-<footer>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('.parallax').parallax();
-        });
-    </script>
-
-</footer>
