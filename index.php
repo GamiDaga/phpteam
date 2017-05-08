6f9da13061cc692ec5ce2f4cf6efd1664b85991c
gami <mati955.daga@gmail.com>
2017-05-08 02:49:23 -0300

    asd2

--- index.php
+++ index.php
@@ -18,6 +18,18 @@
         </div>
 
         <div class="section container ">
+
+          <ul class="right hide-on-med-and-down">
+              <div class="nav-wrapper" >
+                <form>
+                  <div class="input-field">
+                    <input id="search" type="search" required>
+                    <label class="label-icon" for="search">
+                        <i class="material-icons">search</i></label>
+                    <i class="material-icons">close</i>
+                  </div>
+              </form>
+          </ul>
             <h2 class="header">Peliculas</h2>
                 <div class="movie-section">
                     <?php
