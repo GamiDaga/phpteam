c1d0152a2a007c8a729efb463ac5f5cf121add15
Gami <mati955.daga@gmail.com>
2017-06-14 18:17:03 -0300

    asd php

--- functions/functions.php
+++ functions/functions.php
@@ -2,25 +2,26 @@
 require_once('./sql/connect.php');
 require_once('./sql/query.php');
 
-function calification($id){
+function calification($idMovie){
 
     $link = connect();
-    $query=getCalifications($id);
+    $query=getCalifications($idMovie);
 
-    $result=mysqli_query($link, $query);
+    $result = mysqli_query($link, $query);
     //echo '<pre>'; var_dump($result); exit();
-    $row = mysqli_fetch_array($result);
-    //echo '<pre>'; var_dump($row); exit();
-    if ($row != null ) {
-          $tot = array_sum($row);
-          $cant= count($row);
-          //echo '<pre>'; var_dump($cant); exit();
-          $calification=($tot / $cant)." Estrellas";
-          return $calification;
+    // echo '<pre>'; var_dump($result);// exit();
+    if (mysqli_num_rows($result)) {
+        $tot = 0;
+        $cant = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tot += $row['calificacion'];
+            $cant++;
         }
-     else{ 
-     	$calification = 'No hay calificaciones disponibles';
-        return $calification;}
-
+          //echo '<pre>'; var_dump($cant); exit();
+          $calification=($tot / $cant);
+          return $calification;
+    }else{
+        return 0;
+    }
 }
 ?>
